a09f1fb8ead15e33f17a5f543ead17561b89ba1f
Taysir Barakat <taysir.m.barakat@gmail.com>
2024-03-12 10:51:37 +0000

    Commenting

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,14 @@
+# Initialise the loop
 while True:
+
+    # Prompt the user to enter a sentence or end the program
     sentence = input("Please enter a sentence to be printed, or type \"End\" \
 to end the program.")
 
+    # Check if the loop should be exited
     if sentence == "End":
         break
 
+    # Prints the sentence
     else:
         print(sentence)
