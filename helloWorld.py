0405d4aa7d5881b0fadf50772ea840ddf768fc0f
Taysir Barakat <taysir.m.barakat@gmail.com>
2024-03-12 10:46:46 +0000

    Fixed Issue 1 regarding looping the code.

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,9 @@
-sentence = input("Please enter a sentence to be printed.")
-print(sentence)
+while True:
+    sentence = input("Please enter a sentence to be printed, or type \"End\" \
+to end the program.")
+
+    if sentence == "End":
+        break
+
+    else:
+        print(sentence)
